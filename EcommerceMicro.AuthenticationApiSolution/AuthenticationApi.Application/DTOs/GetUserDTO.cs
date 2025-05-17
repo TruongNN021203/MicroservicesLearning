9ed9efdfe81c39d0a7183774a8b9277c16f7ef3e
truongnguyen <truongnn021203@gmail.com>
2025-05-17 14:51:44 +0700

    create order product authentication services

---
+++ EcommerceMicro.AuthenticationApiSolution/AuthenticationApi.Application/DTOs/GetUserDTO.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationApi.Application.DTOs;
+
+public record GetUserDTO(
+    int Id,
+    [Required] string Name,
+    [Required] string TelephoneNumber,
+    [Required] string Address,
+    [Required, EmailAddress] string Email
+);
